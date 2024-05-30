0cc9041332aa7c655ccfc8511de5bfb71dfe17e7
291026192@qq.com <291026192@qq.com>
2024-05-30 12:57:22 +0800

    5.30

--- src/test/java/cn/javaex/ApplicationTests.java
+++ src/test/java/cn/javaex/ApplicationTests.java
@@ -1,4 +1,4 @@
-package cn.javaex.project_manage;
+package cn.javaex;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ProjectManageApplicationTests {
+public class ApplicationTests {
 	
 	@Test
 	public void contextLoads() {
